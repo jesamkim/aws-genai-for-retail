186cbebaebad821412333a577b9a16fc5f6d3938
Jesam <jesamkim@gmail.com>
2023-11-10 10:44:30 +0000

    add 4_lab contents

---
+++ 4_lab/streamlit-inpainting-exmaple/mask_image_app.py
@@ -0,0 +1,106 @@
+import streamlit as st
+from PIL import Image, ImageDraw
+from streamlit_drawable_canvas import st_canvas
+import io
+import json
+import mask_image_lib as glib
+
+# 함수 정의는 모든 호출 뒤에 위치해야 합니다.
+def convert_mask_data_to_image_bytes(mask_data, image_shape, stroke_width=50):
+    # JSON 데이터에서 캔버스 객체 추출
+    mask_data = mask_data.get('objects', []) if isinstance(mask_data, dict) else []
+    # RGB 모드로 흰색 배경을 가진 마스크 이미지를 생성합니다.
+    mask_image = Image.new("RGB", image_shape, (255, 255, 255))
+    draw = ImageDraw.Draw(mask_image)
+
+    for obj in mask_data:
+        if obj.get('type') == 'path':
+            path_data = obj.get('path')
+            # 경로 데이터를 (x, y) 좌표 리스트로 변환
+            path_tuples = [(float(p[1]), float(p[2])) for p in path_data if p[0] in ('L', 'M')]
+            # 선 굵기에 따라 검은색 선을 그립니다.
+            for i in range(len(path_tuples) - 1):
+                draw.line([path_tuples[i], path_tuples[i + 1]], fill=(0, 0, 0), width=stroke_width)
+
+    # 바이트로 변환합니다.
+    mask_bytes = io.BytesIO()
+    mask_image.save(mask_bytes, format='PNG')
+    return mask_bytes.getvalue()
+
+
+
+# Streamlit 페이지 설정
+st.set_page_config(layout="wide", page_title="Image Generation with Masking")
+
+# 제목 설정
+st.title("이미지 생성기")
+
+# 세 개의 컬럼 생성
+col1, col2, col3 = st.columns([3, 1, 3])  # col2는 마스크 표시용으로 더 작게 설정
+
+# 이미지 로드
+image = Image.open('../data/1_product.png').convert("RGB")
+
+# 첫 번째 컬럼 (프롬프트 입력 및 캔버스)
+with col1:
+    st.subheader("이미지 프롬프트")
+    prompt_text = st.text_area("프롬프트를 입력하세요:", height=100)
+    
+    # 캔버스 위젯 설정
+    #stroke_width = st.slider("선 굵기:", 1, 50, 20)
+    stroke_width = 50
+    stroke_color = "#000000"  # 선 색 설정
+    bg_color = "#ffffff"    # 배경색 설정
+    drawing_mode = st.checkbox("그리기 모드", True)
+    
+    # 캔버스 위젯 생성
+    canvas_result = st_canvas(
+        fill_color="rgba(255, 165, 0, 0.3)",
+        stroke_width=stroke_width,
+        stroke_color=stroke_color,
+        background_color=bg_color,
+        background_image=image,
+        update_streamlit=True,
+        height=657,
+        width=512,
+        drawing_mode="freedraw" if drawing_mode else "transform",
+        key="canvas",
+    )
+
+# 두 번째 컬럼 (마스크 이미지 표시)
+with col2:
+    st.subheader("마스크 이미지")
+    if canvas_result.json_data is not None:
+        # 사용자가 설정한 선 굵기를 전달합니다.
+        mask_bytes = convert_mask_data_to_image_bytes(canvas_result.json_data, image.size, stroke_width)
+        st.image(mask_bytes, caption='마스크 이미지', use_column_width=True)
+        
+    prompt_strenth = st.slider("prompt_strenth", min_value=0.0, max_value=30.0, value=10.0, step=0.1, format='%.1f')
+    generation_step = st.slider("Generation step", min_value=10, max_value=150, value=50, step=1)
+    seed = st.slider("Seed", min_value=0, max_value=300, value=10, step=1)
+
+
+
+# 세 번째 컬럼 (결과 표시)
+with col3:
+    st.subheader("결과")
+    process_button = st.button("생성하기")
+    
+    if process_button:
+        if not prompt_text or not canvas_result.json_data.get('objects'):
+            st.warning("프롬프트와 마스크를 지정해주세요.")
+        else:
+            image_array = canvas_result.image_data
+            if image_array is not None:
+                image_pil = Image.fromarray(image_array.astype('uint8'), 'RGBA')
+                image_bytes = io.BytesIO()
+                image_pil.save(image_bytes, format='PNG')
+                
+                mask_bytes = convert_mask_data_to_image_bytes(canvas_result.json_data, image_pil.size)
+                
+                with st.spinner("이미지를 생성하는 중..."):
+                    generated_image = glib.get_image_response(prompt_text, mask_bytes, prompt_strenth, generation_step, seed)
+                
+                st.image(generated_image, caption='생성된 이미지', use_column_width=True)
+            else:
+                st.error("이미지 데이터가 없습니다.")
---
+++ 4_lab/streamlit-inpainting-exmaple/mask_image_lib.py
@@ -0,0 +1,94 @@
+# 필요한 라이브러리를 import 합니다.
+import io
+import os
+import boto3
+import json
+import base64
+from io import BytesIO
+from PIL import Image, ImageDraw, ImageFilter
+# Pillow 버전에 따라 ImageResampling을 다르게 가져올 수 있습니다.
+try:
+    from PIL import ImageResampling
+except ImportError:
+    # Pillow 버전이 낮아 ImageResampling을 직접 가져올 수 없는 경우
+    # Image.Resampling으로 대체할 수 있습니다.
+    from PIL.Image import Resampling as ImageResampling
+
+
+
+# AWS 세션 설정
+session = boto3.Session(
+    profile_name=os.environ.get("BWB_PROFILE_NAME")
+)
+
+# Bedrock 클라이언트 생성
+bedrock = session.client(
+    service_name='bedrock-runtime',
+    region_name=os.environ.get("BWB_REGION_NAME"),
+    endpoint_url=os.environ.get("BWB_ENDPOINT_URL")
+)
+
+# Stable Diffusion 모델 ID
+bedrock_model_id = "stability.stable-diffusion-xl-v0"
+
+
+
+# 응답 페이로드로부터 이미지를 추출하는 함수
+def get_response_image_from_payload(response):
+    payload = json.loads(response.get('body').read())
+    images = payload.get('artifacts')
+    image_data = base64.b64decode(images[0].get('base64'))
+    return BytesIO(image_data)
+
+
+
+# 이미지의 크기를 64의 배수로 조정하는 함수
+def resize_image_to_model_compatible(image, base=64):
+    # 이미지의 너비와 높이를 64의 배수로 조정
+    new_width = ((image.width + base - 1) // base) * base
+    new_height = ((image.height + base - 1) // base) * base
+    resized_image = image.resize((new_width, new_height), Image.Resampling.LANCZOS)
+    return resized_image
+
+
+
+
+# 이미지 응답을 가져오는 함수
+def get_image_response(prompt_content, mask_bytes, prompt_strenth, generation_step, seed):
+    # 요청 바디 설정
+    request_body = {
+        "text_prompts": [{"text": prompt_content}],
+        "cfg_scale": prompt_strenth,
+        "seed": seed,
+        "steps": generation_step,
+        "mask_source": "MASK_IMAGE_BLACK"
+    }
+
+    # 원본 이미지 처리
+    #image = Image.open(BytesIO(image_bytes))
+    image = Image.open('../data/1_product.png').convert("RGB")
+    resized_image = resize_image_to_model_compatible(image)
+    image_bytes_io = BytesIO()
+    resized_image.save(image_bytes_io, format='PNG')
+    encoded_image = base64.b64encode(image_bytes_io.getvalue()).decode('utf-8')
+    request_body["init_image"] = encoded_image
+    
+
+    # 마스크 이미지 처리
+    mask_image = Image.open(BytesIO(mask_bytes))
+    resized_mask_image = resize_image_to_model_compatible(mask_image) # 수정된 부분
+    mask_bytes_io = BytesIO()
+    resized_mask_image.save(mask_bytes_io, format='PNG')
+    encoded_mask_image = base64.b64encode(mask_bytes_io.getvalue()).decode('utf-8')
+    request_body["mask_image"] = encoded_mask_image
+
+    
+    #print(request_body)
+    
+    # 모델 호출
+    response = bedrock.invoke_model(body=json.dumps(request_body), modelId=bedrock_model_id)
+    
+    # 생성된 이미지 반환
+    output = get_response_image_from_payload(response)
+    return output
+
